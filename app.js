a3d087601854774f6da0e5779984895fef7d0c4d
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-13 18:22:28 -0700

    display scoreboard working!

--- app.js
+++ app.js
@@ -7,23 +7,31 @@ const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 let userGuess;
+// let gameState = 'resultState';
 
 const userGuessDisplay = document.getElementById('user-guess-display');
 const ghostGuessDisplay = document.getElementById('ghost-guess-display');
 const resultDisplay = document.getElementById('result-display');
 
-// let userWins = 0;
-// let ghostWins = 0;
-// let draws = 0;
+let userWins = 0;
+let ghostWins = 0;
+let draws = 0;
+// let totalGames;
 
 const ghostGuess = ['rock', 'paper', 'scissors'];
 
 /* Actions */
+function loadPage() {
+    // displayGuess();
+    // displayResults();
+    displayScoreboard();
+}
 
 rock.addEventListener('click', () => {
     const randNum = Math.floor(Math.random() * ghostGuess.length);
     ghostGuessDisplay.textContent = ghostGuess[randNum];
-    userGuess === 'rock';
+    // gameState = resultState;
+    userGuess = 'rock';
     userGuessDisplay.textContent = 'rock';
 
     getResult();
@@ -34,6 +42,7 @@ rock.addEventListener('click', () => {
 paper.addEventListener('click', () => {
     const randNum = Math.floor(Math.random() * ghostGuess.length);
     ghostGuessDisplay.textContent = ghostGuess[randNum];
+    // gameState = 'resultState';
     userGuess = 'paper';
     userGuessDisplay.textContent = 'paper';
 
@@ -45,6 +54,7 @@ paper.addEventListener('click', () => {
 scissors.addEventListener('click', () => {
     const randNum = Math.floor(Math.random() * ghostGuess.length);
     ghostGuessDisplay.textContent = ghostGuess[randNum];
+    // gameState = resultState;
     userGuess = 'scissors';
     userGuessDisplay.textContent = 'scissors';
 
@@ -58,33 +68,62 @@ scissors.addEventListener('click', () => {
 function getResult() {
     if (ghostGuessDisplay.textContent === userGuess) {
         resultDisplay.textContent = 'its a draw!';
+        displayScoreboard();
+        draws++;
     }
     if (ghostGuessDisplay.textContent === 'rock' && userGuess === 'paper') {
         resultDisplay.textContent = 'you win!';
+        displayScoreboard();
+        userWins++;
     }
     if (ghostGuessDisplay.textContent === 'rock' && userGuess === 'scissors') {
         resultDisplay.textContent = 'ghost wins!';
+        displayScoreboard();
+        ghostWins++;
     }
     if (ghostGuessDisplay.textContent === 'paper' && userGuess === 'scissors') {
         resultDisplay.textContent = 'you win!';
+        displayScoreboard();
+        userWins++;
     }
     if (ghostGuessDisplay.textContent === 'paper' && userGuess === 'rock') {
-        resultDisplay.textContent = 'ghost wins!!';
+        resultDisplay.textContent = 'ghost wins!';
+        displayScoreboard();
+        ghostWins++;
     }
     if (ghostGuessDisplay.textContent === 'scissors' && userGuess === 'rock') {
-        resultDisplay.textContent.textContent = 'you win!';
+        resultDisplay.textContent = 'you win!';
+        displayScoreboard();
+        userWins++;
     }
     if (ghostGuessDisplay.textContent === 'scissors' && userGuess === 'paper') {
-        resultDisplay.textContent = 'ghost wins!!';
+        resultDisplay.textContent = 'ghost wins!';
+        displayScoreboard();
+        ghostWins++;
     }
     // console.log('getResult');
 }
 /* Components */
 
 /* Component */
+const userWinsDisplay = document.getElementById('user-wins-display');
+const ghostWinsDisplay = document.getElementById('ghost-wins-display');
+const drawsDisplay = document.getElementById('draws-display');
+// const totalGamesDisplay = document.getElementById('total-games');
+
+function displayScoreboard() {
+    userWinsDisplay.textContent = userWins;
+    ghostWinsDisplay.textContent = ghostWins;
+    drawsDisplay.textContent = draws;
+    // totalGamesDisplay.textContent = totalGames;
+}
 // get DOM
 // display
 // event listeners
 
 /* Run page load code */
-// loadPage();
+loadPage();
+// function playAgain() {
+//     // gameState = 'guess';
+//     loadPage();
+// }
